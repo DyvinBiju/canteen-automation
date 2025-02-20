2ceff5cb811c1c9ee986b00392d4adbf8d5ed248
DyvinBiju <dyvinbiju@gmail.com>
2025-02-20 15:38:59 +0530

    second model added

--- canteen_project/canteenapp/admin.py
+++ canteen_project/canteenapp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from.models import FoodItems
+from.models import FoodItems,orders
 admin.site.register(FoodItems)
+
+admin.site.register(orders)
--- canteen_project/canteenapp/models.py
+++ canteen_project/canteenapp/models.py
@@ -3,9 +3,12 @@ from django.contrib.auth.models import User
 # Create your models here.
 class FoodItems(models.Model):
     food_items = models.CharField(max_length=30)
-    quantity = models.IntegerField()
+    f_stock = models.IntegerField()
     price = models.DecimalField(max_digits=10,decimal_places=2)
-    s_id = models.ForeignKey(User,on_delete=models.CASCADE)
+    student = models.ForeignKey(User,on_delete=models.CASCADE)
     
-
-    
+class orders(models.Model):
+    no_of_items_ordered = models.IntegerField()
+    total_price = models.IntegerField()
+    student = models.ForeignKey(User,on_delete=models.CASCADE)
+    food = models.ForeignKey(FoodItems,on_delete=models.CASCADE)
